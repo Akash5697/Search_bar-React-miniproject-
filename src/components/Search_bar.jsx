3d1f8d9dd396c9e4dce7783e8c35ef8895e1375c
Akash goswami <akashgoswami5697@gmail.com>
2025-02-10 11:28:19 +0530

    n

--- src/components/Search_bar.jsx
+++ src/components/Search_bar.jsx
@@ -2,27 +2,27 @@ import React, { useState } from 'react'
 import "./Search_Bar.css"
 import { FaSearch } from "react-icons/fa";
 
-const Search_bar = ({setResults}) => {
-const [input,setInput] = useState("");
+const Search_bar = ({ setResults }) => {
+    const [input, setInput] = useState("");
 
-const fatchdata = (value)=>{
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response)=>response.json())
-    .then((json)=>{
-       const results = json.filter((user)=>{
-        return value &&
-        user &&
-        user.name &&
-        user.name.toLowerCase().includes(value);
-       });
-       setResults(results);
-    })
-}
+    const fatchdata = (value) => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => response.json())
+            .then((json) => {
+                const results = json.filter((user) => {
+                    return value &&
+                        user &&
+                        user.name &&
+                        user.name.toLowerCase().includes(value);
+                });
+                setResults(results);
+            })
+    }
 
-const handelchange =(value) => {
-    setInput(value);
-    fatchdata(value);
-}
+    const handelchange = (value) => {
+        setInput(value);
+        fatchdata(value);
+    }
 
     return (
         <>
@@ -31,7 +31,7 @@ const handelchange =(value) => {
                 <div className='search_container' >
                     <div className='search-box' >
                         <FaSearch />
-                        <input type="text" placeholder='Type to search...' value={input} onChange={(e)=>handelchange(e.currentTarget.value)} />
+                        <input type="text" placeholder='Type to search...' value={input} onChange={(e) => handelchange(e.currentTarget.value)} />
                     </div>
                 </div>
             </div>
